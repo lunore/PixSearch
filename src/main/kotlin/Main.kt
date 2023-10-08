6ea6ae5b20c17709d373188b20ffce98cfbd3f49
Diana L <dlehmann@edu.cdv.pl>
2023-10-08 21:18:01 +0200

    test commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+/*   val input = readln()
+   var output = ""
+   output = when(input.lowercase()) {
+      "opona" -> "Felga"
+      "maska" -> "Szyba"
+      "test", "cv", "elektronika" -> "COŚ"
+      else -> "Błąd"
+   }
+   println(output)*/
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+   println("Wpisz liczbe: ")
+   var number: Int
+   try {
+      number = readln().toInt()
+   } catch (e: NumberFormatException) {
+      println("To nie jest liczba")
+      return
+   } catch (e: IllegalAccessException) {
+
+   } finally {
+       
+   }
+
+   var output = ""
+   output = when {
+      number > 0 -> "Liczba większa od 0"
+      number == 0 -> "Liczba równa 0"
+      else -> "Liczba mniejsza od 0"
+   }
+   println(output)
 }
